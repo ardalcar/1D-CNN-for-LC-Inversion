114bbdb95626b3a6fd3149afe06b60838896a0e1
MirkoCerci <mirko.cerci@gmail.com>
2023-07-07 17:18:22 +0200

    ottimizzato

--- CNN_Pytorch_grid_search.py
+++ CNN_Pytorch_grid_search.py
@@ -37,7 +37,6 @@ class NeuralNetwork(nn.Module):
     def __init__(self, input_channels_C1, filter_size_C1, kernel_size_C1, kernel_size_M1, 
                  padding_M1, input_channels_C2, filter_size_C2, kernel_size_C2, kernel_size_M2, 
                  hidden_units, output_units):
-       # super(NeuralNetwork, self).__init__()
         super().__init__()
         self.conv1 = nn.Conv1d(input_channels_C1, int(filter_size_C1), int(kernel_size_C1))
         self.maxpool1 = nn.MaxPool1d(kernel_size_M1, padding_M1)
@@ -63,19 +62,36 @@ class NeuralNetwork(nn.Module):
     
 # modello con skorch
 model = NeuralNetRegressor(
-    NeuralNetwork(input_channels_C1=1, filter_size_C1=1., kernel_size_C1=1, kernel_size_M1=1, padding_M1=1, 
-                      input_channels_C2=1, filter_size_C2=1, kernel_size_C2=1, kernel_size_M2=1, hidden_units=2400, 
-                      output_units=6).to(device),
-    criterion = nn.MSELoss,optimizer = optim.Adam,
+    module=NeuralNetwork(
+    input_channels_C1=1, 
+    filter_size_C1=1, 
+    kernel_size_C1=1, 
+    kernel_size_M1=1, 
+    padding_M1=1, 
+    input_channels_C2=1, 
+    filter_size_C2=1, 
+    kernel_size_C2=1, 
+    kernel_size_M2=1, 
+    hidden_units=2400,  
+    output_units=6),
+    criterion = nn.MSELoss, 
+    optimizer = optim.Adam,
+    batch_size = 25,
+    max_epochs = 10,
+    optimizer__lr = 0.001,
     verbose = False
     )
-print(model)
+print(model.initialize)
 
 
 # definisce i parametri del grid search
 param_grid = {
-    'batch_size': [10, 100],
-    'max_epochs': [10, 20]
+    #'batch_size' : [25, 30, 35]
+    #'optimizer__lr' : [0.001, 0.01, 0.1, 0.2, 0.3],    #--> 0.001
+    #'optimirer__momentum' : [0.0, 0.2, 0.4, 0.6, 0.8, 0.9] 
+    #'optimizer': [optim.SGD, optim.RMSprop, optim.Adagrad, optim.Adadelta, #--> optim.Adam
+    #              optim.Adam, optim.Adamax, optim.NAdam]
+
 }
 
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
@@ -89,47 +105,6 @@ params = grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
     print("%f (%f) with: %r" % (mean, stdev, param))
 
-
-# Definisci il numero di epoche, il learning rate e altre iperparametri
-#num_epochs = 10
-#learning_rate = 0.001
-#batch_size = 2400
-
-
-# Creazione di un'istanza del modello
-#model = NeuralNetwork(input_channels_C1=1, filter_size_C1=1., kernel_size_C1=1, kernel_size_M1=1, padding_M1=1, 
-#                      input_channels_C2=1, filter_size_C2=1, kernel_size_C2=1, kernel_size_M2=1, hidden_units=2400, 
-#                      output_units=6).to(device)
-#print(model)
-
-# Definizione ottimizzatore e la loss
-#optimizer = torch.optim.Adam(model.parameters(), learning_rate)
-#criterion = nn.MSELoss()
-
-# Definisci il dataloader per caricare i dati di addestramento
-#train_dataset = torch.utils.data.TensorDataset(inputs, labels)
-#train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-# Ciclo di addestramento
-#for epoch in range(num_epochs):
-#    model.train()
-#    total_loss = 0
-
-#    for batch in train_dataloader:
-#        batch_inputs, batch_labels = batch
-#        batch_inputs = batch_inputs.to(device)
-#        batch_labels = batch_labels.to(device)
-
-#        optimizer.zero_grad()
-#        outputs = model(batch_inputs)
-#        loss = criterion(outputs, batch_labels)
-#        loss.backward()
-#        optimizer.step()
-
-#        total_loss += loss.item()
-
-#    avg_loss = total_loss / len(train_dataloader)
-#    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss}")
 
 # Salva il modello addestrato
 #torch.save(model.state_dict(), "modello_addestrato.pth")
