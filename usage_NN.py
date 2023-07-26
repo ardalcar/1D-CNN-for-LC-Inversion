1a1ed1477614c6ea3d16c773af8eb57d93078ff5
MirkoCerci <mirko.cerci@gmail.com>
2023-07-26 13:09:41 +0200

    use gpu 1

--- usage_NN.py
+++ usage_NN.py
@@ -10,7 +10,7 @@ modello_addestrato = P2.DataParallelModel()
 modello_addestrato.load_state_dict(torch.load("modello_addestrato.pth"))
 
 input_da_valutare = P2.inputs
-input_da_valutare = input_da_valutare.to(P2.device(1))
+input_da_valutare = input_da_valutare.to(P2.device)
 
 modello_addestrato.eval()
 previsioni = modello_addestrato(input_da_valutare)
