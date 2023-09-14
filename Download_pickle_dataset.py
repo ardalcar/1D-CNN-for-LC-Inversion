07f3055427cab8675db75ebe5cf085f7c4eaad03
ardalcar <mirko.cerci@gmail.com>
2023-09-14 14:44:31 +0200

    ok

---
+++ Download_pickle_dataset.py
@@ -0,0 +1,10 @@
+import gdown
+
+URL = "https://drive.google.com/file/d/1grK_wTpl6aOk0O81E-rgVjldZXvp0peO/view?usp=sharing"
+file_name = "X2"
+gdown.download(URL,file_name, fuzzy=True)
+
+URL = "https://drive.google.com/file/d/1WNkVNoTm5zap0vM1yDz55kcowBhWMqjA/view?usp=sharing"
+file_name = "y2"
+gdown.download(URL,file_name, fuzzy=True)
+
