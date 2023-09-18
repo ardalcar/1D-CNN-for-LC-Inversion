998c26e67cf53415f523d9adf798492802821258
ardalcar <mirko.cerci@gmail.com>
2023-09-18 15:00:06 +0200

    100 epoch

--- CNN_Py3.py
+++ CNN_Py3.py
@@ -78,7 +78,7 @@ else:
 # iperparametri
 lr = 0.2       # learning rate
 momentum = 0.001 # momentum
-max_epoch = 40       # numero di epoche
+max_epoch = 100       # numero di epoche
 batch_size = 20  # batch size
 scaler = GradScaler()
 
