026045ef5eafb2e224bcc659b2b75c660428f9e3
ardalcar <mirko.cerci@gmail.com>
2023-10-24 11:50:07 +0200

    new dataset 10k LC

--- Download_pickle_dataset.py
+++ Download_pickle_dataset.py
@@ -1,10 +1,10 @@
 import gdown
 
-URL = "https://drive.google.com/file/d/1-9rVksCfoEI-huDxEzD2FX9O-zb6GILU/view?usp=drive_link"
+URL = "https://drive.google.com/file/d/1K_XdB1ln5RethRj1QKsmvGZDhCwxSsZL/view?usp=drive_link"
 file_name = "./dataCNN/X2"
 gdown.download(URL,file_name, fuzzy=True)
 
-URL = "https://drive.google.com/file/d/1-HDZUqP3D3xvl5O3nE7ORk8yKBqtQpjO/view?usp=drive_link"
+URL = "https://drive.google.com/file/d/1QbV-5qQetL6MfNbKDJwbzp-YxlkmsvuR/view?usp=drive_link"
 file_name = "./dataCNN/y2"
 gdown.download(URL,file_name, fuzzy=True)
 
