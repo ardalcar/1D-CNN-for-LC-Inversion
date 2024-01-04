0107bd7d13c5a23cb2eb8bb7ac229f4fa00a3883
ardalcar <mirko.cerci@gmail.com>
2024-01-04 10:23:46 +0100

    #5.2

--- RNN3.py
+++ RNN3.py
@@ -51,7 +51,7 @@ print(net)
 # iperparametri
 lr = 0.2          # learning rate
 momentum = 0.001  # momentum
-max_epoch = 100    # numero di epoche
+max_epoch = 1000    # numero di epoche
 batch_size = 128  # batch size
 scaler = GradScaler()
 
