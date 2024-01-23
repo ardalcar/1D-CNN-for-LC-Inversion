394871d18f8deb39e0a4c42c6d49415c3dc59c87
ardalcar <mirko.cerci@gmail.com>
2024-01-23 15:20:45 +0100

    41#

--- Download_pickle_dataset.py
+++ Download_pickle_dataset.py
@@ -2,7 +2,7 @@ import gdown
 
 
 try:
-    a = int(input('selezionare il dataset da scaricare [2/3/4]: '))
+    a = int(input('selezionare il dataset da scaricare [2/3/4/41]: '))
     if a == 2:
         URL = "https://drive.google.com/file/d/1K_XdB1ln5RethRj1QKsmvGZDhCwxSsZL/view?usp=sharing"
         file_name = "./dataCNN/y2"
@@ -30,6 +30,15 @@ try:
         file_name = "./dataCNN/y_sub"
         gdown.download(URL,file_name, fuzzy=True)
     
+    elif a == 41:
+        URL = "https://drive.google.com/file/d/1IsJURYHU5_KhYoLlHmvxKNBTc3JcwsmP/view?usp=sharing"
+        file_name = "./dataCNN/X41"
+        gdown.download(URL,file_name, fuzzy=True)
+
+        URL = "https://drive.google.com/file/d/1dzISl0nBNOP2GKOL_3QMoLWj0sJzGLDk/view?usp=sharing"
+        file_name = "./dataCNN/y41"
+        gdown.download(URL,file_name, fuzzy=True)        
+
     else:
         print('input non valido')
 
