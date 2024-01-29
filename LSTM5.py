350bc3e298bcb9e45b3ee1d8acfbc0758c9fbc35
ardalcar <mirko.cerci@gmail.com>
2024-01-29 11:46:04 +0100

    #1000#

--- LSTM5.py
+++ LSTM5.py
@@ -54,7 +54,7 @@ print(net)
 # iperparametri
 lr = 0.001        # learning rate
 momentum = 0.001  # momentum
-max_epoch = 5  # numero di epoche
+max_epoch = 1000  # numero di epoche
 batch_size = 128  # batch size
 scaler = GradScaler()
 
